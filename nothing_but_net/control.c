29b339cbbd8dd502d1860725d79b718037d3b07a
Jonathan Lee <Jonwlee457@gmail.com>
2016-01-29 22:18:55 -0500

    Spaghetti
    bad bad bad

--- nothing_but_net/control.c
+++ nothing_but_net/control.c
@@ -1,29 +1,32 @@
 #include "../shared/functions.c"
 
 void control() {
-	bool slowDrive = false;
+	//bool slowDrive = false;
 
 	while(true) {
+		basicDrive(vexRT[Ch3],vexRT[Ch2]);
 
 		//Drive
-		if (vexRT[Btn7D] || vexRT[Btn7L] || vexRT[Btn7U] || vexRT[Btn7R] || 
-			vexRT[Btn8D] || vexRT[Btn8L] || vexRT[Btn8U] || vexRT[Btn8R]) slowDrive = true;
-		else slowDrive = false;
+		//if (vexRT[Btn7D] || vexRT[Btn7L] || vexRT[Btn7U] || vexRT[Btn7R] ||
+		//	vexRT[Btn8D] || vexRT[Btn8L] || vexRT[Btn8U] || vexRT[Btn8R]) slowDrive = true;
+		//else slowDrive = false;
 
-		basicDrive( //ternary operator, if slowDrive is true, power is divided by 2.2
-			slowDrive ? (vexRT[Ch3] / 2.2) : vexRT[Ch3],
-			slowDrive ? (vexRT[Ch2] / 2.2) : vexRT[Ch2]
-			);
+		//basicDrive( //ternary operator, if slowDrive is true, power is divided by 2.2
+		//	slowDrive ? (vexRT[Ch3] / 2.2) : vexRT[Ch3],
+		//	slowDrive ? (vexRT[Ch2] / 2.2) : vexRT[Ch2]
+		//	);
 
 		//Intake (Horizontal and Vertical)
-		if (vexRT[Btn6U]) basicBothIntake(127);
+		if (vexRT[Btn6U]) basicIntake(127);
 
-		else if (vexRT[Btn6D]) basicBothIntake(-127);
+		else if (vexRT[Btn6D]) basicIntake(-127);
 
-		else basicBothIntake(0);
+				else basicIntake(0);
 
 		// Flywheels
-		if (vexRT[Btn5U])  flywheels(127); //rev spin motors
-		else if (vexRT[Btn5D]) flywheels(0);
+		if (vexRT[Btn5U])  flywheels(127,127); //rev spin motors
+		else if (vexRT[Btn5D]) flywheels(64,64);
+		else flywheels(0,0);
 
 	}
+}
--- shared/functions.c
+++ shared/functions.c
@@ -1,21 +1,25 @@
 void basicDrive (int leftPower, int rightPower) {
-	// motor[FLDrive] = motor[BLDrive] = leftPower; //currently commented out bc we lack y cables
-	// motor[FRDrive] = motor[BRDrive] = rightPower;
+  motor[LDrive] = -leftPower;
+  motor{RDrive] = rightPower;
 	}
 
-void basicHorizontalIntake (int power) {
+//void basicHorizontalIntake (int power) {
+//	motor[HIntake] = power;
+//	}
+
+void basicIntake (int power) {
+	motor[VIntake] = -power
 	motor[HIntake] = power;
 	}
 
-void basicVerticalIntake (int power) {
-	motor[BVertIntake] = motor[UVertIntake] = power;
-	}
+//void basicBothIntakes (int power) {
+//	basicHorizontalIntake(power);
+//	basicVerticalIntake((-1)*(power));
+//	}
 
-void basicBothIntakes (int power) {
-	basicHorizontalIntake(power);
-	basicVerticalIntake(power);
-	}
-
-void flywheels (int power) {
-	motor[BLFly] = motor[BRFly] = motor[ULFly] = motor[URFly]= power;
+void flywheels (int UPower, int BPower) {
+	motor[BLFly] = - BPower
+	motor[BRFly] =  BPower;
+	motor[ULFly] = UPower;
+	motor[URFly]= -UPower;
 	}
